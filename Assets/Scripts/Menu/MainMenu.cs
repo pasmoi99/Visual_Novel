9d86ed5a2255d2000f52e20633c6f5a1fcd07c34
lisamiloche <lisa.miloche@gmail.com>
2023-10-09 23:06:45 +0200

    avancement menu
    debut menu option

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -4,30 +4,39 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEditor;
 
 public class MainMenu : MonoBehaviour
 {
-    public Image imageFade;
 
+    public Image imageFade;
+    public List<Button> buttonsList;
+    public Image imageButton;
+    public List<TMPro.TextMeshProUGUI> textList;
+
+    // transition entre les scenes
     public void OnCLickPlay()
     {
         imageFade.DOFade(1, 1.5f).OnComplete(FadeComplete);
+
+        for (int i = 1; i < buttonsList.Count; i++)
+        {
+            buttonsList[i].gameObject.SetActive(false);
+
+            //cacher image et texte des boutons
+            imageButton.DOKill();
+            imageButton.DOFade(0, 0.5f);
+
+            textList[i].DOKill();
+            textList[i].DOFade(0, 0.5f);
+        }
+        
     }
 
-
+    // button play -> changement de scene vers gameplay
    public void FadeComplete()
     {
         SceneManager.LoadScene("SceneThomas");
     }
 
-    void Start()
-    {
-        
-    }
-
- 
-    void Update()
-    {
-        
-    }
 }
